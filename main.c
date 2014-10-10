20623102be4790049e22b7752247d1edfa7874d3
Yuki Abe <abe@soc.ait.kyushu-u.ac.jp>
2014-10-10 15:50:36 +0900

    test

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>             
 int main()                     
 {                              
-    printf("hello, world\n");  
-    printf("hello, world2\n");  
-    printf("hello, world4\n");  
 		return 0;                  
 
 }                          
