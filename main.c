1ee6d9bc2ffb8a341764eb91eb6b72608f068d71
Yuki Abe <abe@soc.ait.kyushu-u.ac.jp>
2014-10-10 14:44:34 +0900

    Add "hello, world2"

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 int main()                     
 {                              
     printf("hello, world\n");  
+    printf("hello, world2\n");  
 		return 0;                  
 
 }                          
